66877288aa41e34fc21f056bbd4da09de11d178b
TheChillCoder <98901984+David-Frank@users.noreply.github.com>
2022-08-14 16:36:50 -0500

    Create index.js

---
+++ front-end-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+2	import ReactDOM from 'react-dom';
+3	import App from './App';
+4	
+5	ReactDOM.render(
+6	  <React.StrictMode>
+7	    <App />
+8	  </React.StrictMode>,
+9	  document.getElementById('root')
+10	);
